575b35a8754c40ad74c17cadadbe50f61ab5c2ab
Ramesh Mhetre <ramesh@eddura.com>
2016-08-07 14:39:23 +0530

    Teitter credentials

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -4,10 +4,10 @@ require 'twitter'
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'YOUR_CONSUMER_KEY'
-  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-  config.access_token = 'YOUR_OAUTH_TOKEN'
-  config.access_token_secret = 'YOUR_OAUTH_SECRET'
+  config.consumer_key = 'QJv3Mn7HHLf4YSLRyNed7eCpN'
+  config.consumer_secret = 'ZaRjuu4D0nTFZIYMohi8opRt6IdTrAbcCJW5UZM0zfPURqZIM4'
+  config.access_token = '434845940-F4ffJaa4px1cOanBhnEvw5zCstRpC6oG9lkH3pP1'
+  config.access_token_secret = 'I9SF2ssnTmmpOVDUIzktq7LdqUTby4KIYVlrvvAZzgEiB'
 end
 
 search_term = URI::encode('#todayilearned')
